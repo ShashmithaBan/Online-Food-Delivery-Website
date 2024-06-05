5752756d0833d619dd184bcd0ea33dc50d85da85
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-05 23:59:41 +0530

    updated

--- Frontend/ekbarfoods/src/AdminComponent/Admin.jsx
+++ Frontend/ekbarfoods/src/AdminComponent/Admin.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import { AdminSidebar } from './AdminSidebar'
 
-const menu = [
-    {title:"Dashboard" , icon:},
-    {title:"Orders" , icon:},
-    {title:"Menu" , icon:},
-    {title:"Foodcategory" , icon:},
-    {title:"Ingredients" , icon:},
-    {title:"Events" , icon:<EventIcon/>},
-    {title:"Details" , icon:},
-    {title:"Logout" , icon:<LogoutIcon/>}
-  ]
+
 
 export const Admin = () => {
   return (
