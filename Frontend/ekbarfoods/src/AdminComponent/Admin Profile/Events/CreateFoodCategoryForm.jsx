fc2fb23b7b42c7e82c7e751b3dde08c5ef3dce4f
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-13 23:16:14 +0530

    updated

---
+++ Frontend/ekbarfoods/src/AdminComponent/Admin Profile/Events/CreateFoodCategoryForm.jsx
@@ -0,0 +1,46 @@
+import { Button, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { Form } from 'react-router-dom'
+
+export const CreateFoodCategoryForm = () => {
+const[formData , setFormData] = useState({categoryName:"", restaurantId:""})
+  const handleSubmit = () =>{
+
+    const data= {
+      name : formData.categoryName,
+      restaurantId:{
+        id:1,
+      }
+    }
+    console.log(data)
+  }
+
+  const handleInputChange = (e) =>{
+     const{name,value} = e.target
+     setFormData({
+      ...formData, [name]:value
+     })
+  }
+  return (
+    <div className=''>
+      <div className="p-5 ">
+        <h1 className="text-gray-400 text-center text-green-500 font-bold text-xl pb-10">Create Category</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
+        <TextField
+  fullWidth
+  id="categoryName"
+  name="categoryName"
+  label="Category Name"
+  variant="outlined"
+  onChange={handleInputChange}
+  value={FormData.categoryName}
+  
+/>
+<Button type = "submit" variant="contained">
+  Create Category
+</Button>
+        </form>
+      </div>
+    </div>
+  )
+}
--- Frontend/ekbarfoods/src/AdminComponent/Admin Profile/Events/Events.jsx
+++ Frontend/ekbarfoods/src/AdminComponent/Admin Profile/Events/Events.jsx
@@ -1,10 +1,119 @@
 import React from 'react'
 import { EventCard } from '../../../Components/Profile/Cards/EventCard'
-import { Button } from '@mui/material'
+import { Box, Button, Grid, Modal } from '@mui/material'
+import { CreateEventForm } from './CreateEventForm'
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'black',
+  shadows:"1px",
+  boxShadow: 24,
+  p: 4,
+  color:"black"
+};
+
 export const Events = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [formValues,setFormValues] = React.useState({
+    image:"",
+    location:"",
+    name:"",
+    startedAt:"",
+    endsAt:null
+  })
+  const handleSubmit = () =>{
+    
+  }
+  const handleFormChange = (e) =>{
+    setFormValues({...formValues,[e.target.name]:e.target.value})
+  }
+  const handleDateChange = (date,dateType) =>{
+      const formatedDate = days(date)
+  }
   return (
+    
     <div className='flex flex-col items-center pt-5'>
-      <Button variant='contained'>CREATE NEW EVENTS</Button>
+      <Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <form onSubmit={handleSubmit}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+        <TextField
+  fullWidth
+ 
+  name="image"
+  label="image URL"
+  variant="outlined"
+  onChange={handleFormChange}
+  value={formValues.image}
+  
+/>
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+  fullWidth
+ 
+  name="location"
+  label="Location"
+  variant="outlined"
+  onChange={handleFormChange}
+  value={formValues.location}
+  
+/>
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+  fullWidth
+ 
+  name="name"
+  label="Name"
+  variant="outlined"
+  onChange={handleFormChange}
+  value={formValues.name}
+  
+/>
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+  fullWidth
+ 
+  name="location"
+  label="Location"
+  variant="outlined"
+  onChange={handleFormChange}
+  value={formValues.location}
+  
+/>
+        </Grid>
+        <Grid item xs={12}>
+        <TextField
+  fullWidth
+ 
+  name="location"
+  label="Location"
+  variant="outlined"
+  onChange={handleFormChange}
+  value={formValues.location}
+  
+/>
+        </Grid>
+      </Grid>
+    </form>
+
+  </Box>
+</Modal>
+      <Button variant='contained' onClick={handleOpen}>CREATE NEW EVENTS</Button>
       <div className="flex flex-wrap justify-center">
       {[1,1,1].map((i)=><EventCard/>)}
       </div>
