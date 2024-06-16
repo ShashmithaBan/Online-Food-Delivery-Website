ae8a882a88663e52841c8134a83680f20f644f40
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-16 21:52:15 +0530

    updated

--- Frontend/ekbarfoods/src/AdminComponent/Admin Profile/Ingredients/CreateIngredientCategoryForm.jsx
+++ Frontend/ekbarfoods/src/AdminComponent/Admin Profile/Ingredients/CreateIngredientCategoryForm.jsx
@@ -1,39 +1,53 @@
-import { Button, TextField } from '@mui/material'
-import React, { useState } from 'react'
-import { Form } from 'react-router-dom'
+import { Button, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createIngredientsCategory } from '../../../Components/State/Ingredient/Action';
 
 export const CreateIngredientCategoryForm = () => {
-const[formData , setFormData] = useState({name:""})
-  const handleSubmit = () =>{
-    console.log(formData)
-  }
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({ name: "" });
 
-  const handleInputChange = (e) =>{
-     const{name,value} = e.target
-     setFormData({
-      ...formData, [name]:value
-     })
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    const data = {
+      name: formData.name,
+      restaurantId: {
+        id: 1,
+      },
+    };
+    console.log("Ingredient Categories:", data);
+    dispatch(createIngredientsCategory({
+      data: data,
+      jwt: localStorage.getItem("jwt"),
+    }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData, [name]: value,
+    });
+  };
+
   return (
     <div className=''>
-      <div className="p-5 ">
+      <div className="p-5">
         <h1 className="text-gray-400 text-center text-green-500 font-bold text-xl pb-10">Ingredient Category</h1>
         <form onSubmit={handleSubmit} className='flex flex-col gap-5'>
-        <TextField
-  fullWidth
-  id="name"
-  name="name"
-  label="Name"
-  variant="outlined"
-  onChange={handleInputChange}
-  value={formData.name}
-  
-/>
-<Button type = "submit" variant="contained">
-  Create Category
-</Button>
+          <TextField
+            fullWidth
+            id="name"
+            name="name"
+            label="Name"
+            variant="outlined"
+            onChange={handleInputChange}
+            value={formData.name}
+          />
+          <Button type="submit" variant="contained">
+            Create Category
+          </Button>
         </form>
       </div>
     </div>
-  )
-}
+  );
+};
