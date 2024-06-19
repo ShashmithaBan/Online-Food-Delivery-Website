f6eefdb0ee8e7339e8e82249aca34c51470d37ae
Shashmitha Bandara <139967749+ShashmithaBan@users.noreply.github.com>
2024-06-19 23:38:18 +0530

    updated

--- Frontend/ekbarfoods/src/App.js
+++ Frontend/ekbarfoods/src/App.js
@@ -5,33 +5,27 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import darkTheme from './Theme/DarkTheme'
 
 
-import { useDispatch, useSelector } from 'react-redux';
+import { Provider, useDispatch, useSelector } from 'react-redux';
 import { getUser } from './Components/State/Authentication/Action';
 import { Routers } from './Routes/Routers';
 import { CustomerRoutes } from './Routes/CustomerRoutes';
 import { getRestaurantByUserId } from './Components/State/Restaurant/Action';
+import { store } from './store';
 
 function App() {
-  const dispatch = useDispatch()
-  const jwt = localStorage.getItem("jwt")
-  const {auth}= useSelector(store=>store)
-
-   useEffect(()=>{
-    dispatch(getUser(auth.jwt || jwt))
-    
-  },[auth.jwt]
-  );
-  useEffect(()=>{
-   dispatch(getRestaurantByUserId(auth.jwt || jwt))
-  },[auth.user])
+  
 
 
   return (
     
-    <ThemeProvider theme={darkTheme}>
+<ThemeProvider theme={darkTheme} >
       <CssBaseline/>
-      <Routers/>
+      <Provider store={store}>
+      <Routers/> 
+      </Provider>
     </ThemeProvider>
+   
+    
   );
 }
 
--- Frontend/ekbarfoods/src/Components/Navbar/Navbar.jsx
+++ Frontend/ekbarfoods/src/Components/Navbar/Navbar.jsx
@@ -3,27 +3,27 @@ import { Avatar, Badge, Box, IconButton } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 import { green } from '@mui/material/colors';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import './Navbar.css';
 import Person4Icon from '@mui/icons-material/Person4';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import './Navbar.css';
 
-export const Navbar = () => 
-  {
-    const{auth} = useSelector(store=>store) 
-    const navigate = useNavigate()
-    const handleAvatarClick=()=>{
-      if(auth.user?.role==="ROLE_CUSTOMER"){
-        navigate("/my-profile ")
-      }else{
-        navigate("/admin/restaurant")
-      }
+const Navbar = () => {
+  const { auth } = useSelector(store => store) || { auth: { user: null } }; // Provide a default value to prevent undefined errors
+  const navigate = useNavigate();
+
+  const handleAvatarClick = () => {
+    if (auth?.user?.role === "ROLE_CUSTOMER") {
+      navigate("/my-profile");
+    } else {
+      navigate("/admin/restaurant");
     }
-    
+  };
+
   return (
     <Box className='px-5 sticky top-0 z-[100] py-4 bg-[#40c165] md:py-2 md:px-10 lg:px-20 flex justify-between'>
       <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
-        <li onClick={()=>navigate("/")} className='logo font-semibold text-black-300 text-2xl list-none font-mono'>
+        <li onClick={() => navigate("/")} className='logo font-semibold text-black-300 text-2xl list-none font-mono'>
           EkBar <span className='text-gray-50 font-extrabold'>Foods</span>
         </li>
       </div>
@@ -34,11 +34,15 @@ export const Navbar = () =>
           </IconButton>
         </div>
         <div className="">
-          {auth.user?<Avatar onClick={handleAvatarClick} sx={{ bgcolor: "white", color: green.A700 }}>{auth.user?.fullName[0].toUpperCase( )}</Avatar>:
-          <IconButton onClick={()=>navigate("/account/login")}>
-                <Person4Icon sx={{fontSize:"2rem"}}/>
-          </IconButton>
-          }
+          {auth?.user ? (
+            <Avatar onClick={handleAvatarClick} sx={{ bgcolor: "white", color: green.A700 }}>
+              {auth.user?.fullName[0]?.toUpperCase()}
+            </Avatar>
+          ) : (
+            <IconButton onClick={() => navigate("/account/login")}>
+              <Person4Icon sx={{ fontSize: "2rem" }} />
+            </IconButton>
+          )}
         </div>
         <div>
           <IconButton className=''>
